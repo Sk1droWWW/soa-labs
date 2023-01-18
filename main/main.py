a34878c5732c4b292b4a8a94b89fc4bb295e5a84
v.frolkin <v.frolkin@smartweb.com.ua>
2023-01-18 23:53:35 +0200

    Added nginx routing and scale workers

--- main/main.py
+++ main/main.py
@@ -1,25 +1,29 @@
-from flask import Flask
 import socket
+
 import requests
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return f"Web App with Python Flask by Mykhailo Tokar, container id: {socket.gethostname()}"
 
 
-@app.route('/info', methods = ['GET'])
+@app.route('/info', methods=['GET'])
 def weather():
-    company_response = requests.get("http://company")
-    country_response = requests.get("http://country")
- 
-    company_info = company_response['company'] + ": " + company_response['provided_aid'] + "\n"
-    country_info = country_response['country'] + ": " + country_response['provided_tanks'] + "\n"
-   
+    company_response = requests.get("http://balancer:8000/company").json()
+    country_response = requests.get("http://balancer:8000/country").json()
+
+    company_info = company_response['company'] + \
+        ": " + company_response['provided_aid'] + "\n"
+    country_info = country_response['country'] + \
+        ": " + country_response['provided_tanks'] + "\n"
+
     company_host = "\ncompany_host: " + company_response['company_host']
     country_host = "\ncountry_host: " + country_response['country_host']
-    
+
     return company_info + country_info + company_host + country_host
 
 
