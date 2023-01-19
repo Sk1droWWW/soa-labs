e2b89969f58e75aae0a7510717a2109860dece52
Mykhailo Tokar <mykhailo.tokar@welltech.com>
2023-01-20 00:43:43 +0200

    fix filename

--- ktor-companies/src/main/kotlin/service/plugins/Routing.kt
+++ ktor-companies/src/main/kotlin/service/plugins/Routing.kt
@@ -1,14 +1,17 @@
 package service.plugins
 
-import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
 import service.data.Companies
+import service.data.Company
 import java.io.File
 
-private const val COMPANIES_FILE = "./src/main/resources/companies.json"
+private const val COMPANIES_FILE = "ktor-companies/src/main/resources/companies.json"
 
 fun Application.configureRouting() {
     routing {
